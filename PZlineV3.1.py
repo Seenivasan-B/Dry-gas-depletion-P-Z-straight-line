119a9cb9229e960a4476edbf6892c96f24f8cd0f
Seenivasan Balasubramanian <seeni24vasan@gmail.com>
2024-12-11 13:58:03 +0530

    Update PZlineV3.1.py

--- PZlineV3.1.py
+++ PZlineV3.1.py
@@ -97,11 +97,11 @@ with col3:
 #Section 2 end 
         st.subheader("P/Z Analysis")
         try :
-            pres_in = st.slider(label='Initial Reservoir Pressure (Psia)',min_value=float(uplim-1000), max_value=float(uplim+1000),step=1.,format='%.0f',value=float(uplim))
+            pres_in = st.slider(label='Initial Reservoir Pressure (Psia)',min_value=float(uplim*0.8), max_value=float(uplim*1.2),step=1.,format='%.0f',value=float(uplim))
             res_temp = st.slider(label='Reservoir Temperature (°F)',min_value=60., max_value=250.,step=1.,format='%.0f',value = 100.0)
             gas_sg = st.slider(label='Gas Specific Gravity',min_value=0.5, max_value=.7,step=0.01,format='%0.2f',value=0.6)
-            ogip = st.slider(label='OGIP(Bscf)',min_value=ogip_lim-20, max_value=(ogip_lim+ogip_lim),step=.1,format='%.1f',value=ogip_lim)
-            pres_ab = st.slider(label='Abandonment Pressure (Psia)',min_value=100., max_value=1000.,step=1.,format='%.0f',value=float(uplim/10))
+            ogip = st.slider(label='OGIP(Bscf)',min_value=(ogip_lim*0.5), max_value=(ogip_lim*2),step=.1,format='%.1f',value=ogip_lim)
+            pres_ab = st.slider(label='Abandonment Pressure (Psia)',min_value=(uplim*0.5), max_value=1000.,step=1.,format='%.0f',value=float(uplim/10))
         except:
              print("Provide Proper input")
         st.markdown("---") 
