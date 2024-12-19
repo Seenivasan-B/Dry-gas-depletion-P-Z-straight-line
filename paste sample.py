892ed46e716a564fa6cb8a0d6987e8566e4af12a
Seenivasan Balasubramanian <seeni24vasan@gmail.com>
2024-12-19 13:59:35 +0530

    Update paste sample.py

--- paste sample.py
+++ paste sample.py
@@ -1,24 +1,33 @@
-import streamlit as st
 import pyperclip
 import pandas as pd
+import streamlit as st
+load = st.button("Paste from clipboard")
+if "load_state" not in st.session_state:
+    st.session_state.load_state = False
+if load or st.session_state.load_state:
+    st.session_state.load_state = True
+    try:
+    # Read the clipboard content into a pandas DataFrame
+        
+        text = pyperclip.paste()
+        text = text.split('\n')
+        #st.write(text)
 
-text = pyperclip.paste()
+        data = text
 
-text = text.split('\n')
-#st.write(text)
+        data = [s.split("\t") for s in data if "\t" in s]
+        #st.write(data)
+        # Create a DataFrame
+        df = pd.DataFrame(data, columns=['Index', 'Value'])
 
-data = text
+        # Remove the newline character from the 'Value' column
+        df['Value'] = df['Value'].str.strip('\r')
 
-data = [s.split("\t") for s in data if "\t" in s]
-#st.write(data)
-# Create a DataFrame
-df = pd.DataFrame(data, columns=['Index', 'Value'])
+        # Convert the 'Index' and 'Value' columns to numeric data types
+        df['Index'] = pd.to_numeric(df['Index'])
+        df['Value'] = pd.to_numeric(df['Value'])
 
-# Remove the newline character from the 'Value' column
-df['Value'] = df['Value'].str.strip('\r')
-
-# Convert the 'Index' and 'Value' columns to numeric data types
-df['Index'] = pd.to_numeric(df['Index'])
-df['Value'] = pd.to_numeric(df['Value'])
-
-st.write(df)
+        st.write(df)
+    except Exception as e:
+        st.error("Failed to read data from clipboard. Make sure the clipboard contains only numeric values in two columns.")
+        st.error(f"Error details: {e}")
